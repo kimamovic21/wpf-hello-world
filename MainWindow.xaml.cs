10aa45d35f0706fff70bbcf8017923ea964a38ad
kimamovic21 <imamovic.kerim@gmail.com>
2025-02-23 20:52:07 +0100

    PasswordBox

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -17,47 +17,5 @@ namespace WPFHelloWorld
         {
             InitializeComponent();
         }
-
-        //private void cbCheese_Checked(object sender, RoutedEventArgs e)
-        //{
-        //    lbCheese.Background = Brushes.Yellow;
-        //}
-
-        //private void cbCheese_Unchecked(object sender, RoutedEventArgs e)
-        //{
-        //    lbCheese.Background = Brushes.White;
-        //}
-
-        private void cbParentCheckedChanged(object sender, RoutedEventArgs e)
-        {
-            bool newValue = (cbParent.IsChecked == true);
-            cbCheese.IsChecked = newValue;
-            cbTuna.IsChecked = newValue;
-            cbPepperoni.IsChecked = newValue;
-            cbOlives.IsChecked = newValue;
-        }
-
-        private void cbTopingsCheckedChanged(object sender, RoutedEventArgs e)
-        {
-            cbParent.IsChecked = null;
-            if(
-                (cbCheese.IsChecked == true) && 
-                (cbTuna.IsChecked == true) &&
-                (cbPepperoni.IsChecked == true) &&
-                (cbOlives.IsChecked == true)
-            )
-            {
-                cbParent.IsChecked = true;
-            }
-            if (
-                (cbCheese.IsChecked == false) &&
-                (cbTuna.IsChecked == false) &&
-                (cbPepperoni.IsChecked == false) &&
-                (cbOlives.IsChecked == false)
-            )
-            {
-                cbParent.IsChecked = false;
-            }
-        }
     }
 }
