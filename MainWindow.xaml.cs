09dedd5da27fdacf89ddfe357c61af00f091c3b9
kimamovic21 <imamovic.kerim@gmail.com>
2025-02-23 14:33:53 +0100

    Labels

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -16,22 +16,6 @@ namespace WPFHelloWorld
         public MainWindow()
         {
             InitializeComponent();
-
-            //myTextBlock.Text = "Hello from the cs side!";
-            //myTextBlock.Foreground = Brushes.Blue;
-
-            TextBlock myTb = new TextBlock();
-            myTb.Text = "Hello from the cs side!";
-            myTb.Inlines.Add(" this is added using inlines");
-            myTb.Inlines.Add(new Run(" Run text that i added in Code behind")
-            {
-                Foreground = Brushes.Red,
-                TextDecorations = TextDecorations.Underline,
-            });
-
-            myTb.TextWrapping = TextWrapping.Wrap;
-            myTb.Foreground = Brushes.Blue;
-            this.Content = myTb;
         }
     }
 }
